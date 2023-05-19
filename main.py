fd11e8b797cec60896a52a99f806181ccdf4bb4e
Laxmi Balcha <laxmivijaya@gmail.com>
2023-05-19 16:56:18 -0400

    Check in for Labels

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
-from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
-from kivymd.uix.list import MDList,ThreeLineListItem
-from kivy.uix.scrollview import ScrollView
+from random import randint
 
-class DemoApp(MDApp):
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.vector import Vector
+
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            ball.velocity_x *= -1.1
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # Latest Position = Current Velocity + Current Position
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+# Update - moving the ball by calling the move() and other stuff
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.y > self.height - 50):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and increase the score
+        if self.ball.x < 0:
+            self.ball.velocity_x *= -1
+            self.player1.score +=1
+        # bounce off right
+        if self.ball.x > self.width - 50:
+            self.ball.velocity_x *= -1
+            self.player2.score += 1
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 1 / 4:
+            self.player1.center_y = touch.y
+        if touch.x > self.width * 3 / 4:
+            self.player2.center_y = touch.y
+
+
+class PongApp(App):
     def build(self):
-        screen = Screen()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
-        scroll = ScrollView()
-        list_view = MDList()
-        scroll.add_widget(list_view)
 
-        for i in range(20):
-            items = ThreeLineListItem(text='Item ' + str(i), secondary_text='Hello World', tertiary_text='To Everyone')
-            list_view.add_widget(items)
-
-        screen.add_widget(scroll)
-
-        return screen
-
-DemoApp().run()
+PongApp().run()
