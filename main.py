9eeb1294bf6c2d9286d503dbfa64259d5c2ef74e
Laxmi Balcha <laxmivijaya@gmail.com>
2023-06-02 14:29:33 -0400

    Check in for screenmanager

--- main.py
+++ main.py
@@ -1,44 +1,69 @@
-from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivymd.uix.screen import Screen
-
-
-Window.size = (300, 500)
 
 screen_helper = """
-Screen:
-    BoxLayout:
-        orientation: 'vertical'
-        MDTopAppBar:
-            title: 'Demo Application'
-            left_action_items: [["menu", lambda x: app.navigation_draw()]]
-            right_action_items: [["clock", lambda x: app.navigation_draw()]]
-            elevation: 5
-        MDLabel:
-            text: 'Hello World'
-            halign: 'center'
-            
-        MDBottomAppBar:
-            MDTopAppBar:
-                title: 'Help'
-                left_action_items: [["coffee", lambda x: app.navigation_draw()]]
-                mode: 'end'
-                type: 'bottom'
-                icon: 'language-python'
-                on_action_button: app.navigation_draw()
+ScreenManager:
+    MenuScreen:
+    ProfileScreen:
+    UploadScreen:
+    
+<MenuScreen>:
+    name: 'menu'
+    MDRectangleFlatButton:
+        text: 'Profile'
+        pos_hint: {'center_x':0.5, 'center_y':0.5}
+        on_press: root.manager.current = 'profile'
+    MDRectangleFlatButton:
+        text: 'Upload'
+        pos_hint: {'center_x':0.5,'center_y':0.4}
+        on_press: root.manager.current = 'upload'
+        
+<ProfileScreen>:
+    name: 'profile'
+    MDLabel:
+        text:'Welcome'
+        align: 'center'
+    MDRectangleFlatButton:
+        text: 'Back'
+        pos_hint: {'center_x':0.5, 'center_y':0.2}
+        on_press: root.manager.current = 'menu'
+        
+<UploadScreen>:
+    name: 'upload'
+    MDLabel:
+        text:'Lets Upload some files'
+        align: 'center'
+    MDRectangleFlatButton:
+        text: 'Back'
+        pos_hint: {'center_x':0.5, 'center_y':0.2}
+        on_press: root.manager.current = 'menu'
+        
 """
+
+
+class MenuScreen(Screen):
+    pass
+
+
+class ProfileScreen(Screen):
+    pass
+
+
+class UploadScreen(Screen):
+    pass
+
+
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(ProfileScreen(name='profile'))
+sm.add_widget(UploadScreen(name='upload'))
 
 
 class DemoApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = 'Red'
         screen = Builder.load_string(screen_helper)
-
         return screen
-
-    def navigation_draw(self):
-        print("Navigation")
 
 
 DemoApp().run()
